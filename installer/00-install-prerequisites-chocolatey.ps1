1c59a466c760e44b07b070fff6b8838c9a4cddfc
Zui Young <young@youngenius.com>
2017-06-22 17:13:21 +0800

    Remove hats folder if

--- installer/00-install-prerequisites-chocolatey.ps1
+++ installer/00-install-prerequisites-chocolatey.ps1
@@ -4,6 +4,14 @@
     chocolatey install googlechrome nodejs-lts --no-progress -y
     chocolatey upgrade googlechrome nodejs-lts firefox --no-progress -y
     chocolatey uninstall jdk8 python2 --no-progress -y > $null
+    
+	if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
+        Remove-Item -Recurse -Force -ErrorAction silentlycontinue "${Env:ProgramFiles(x86)}\hats"
+	  }
+	  else {
+	    Remove-Item -Recurse -Force  -ErrorAction silentlycontinue "${Env:ProgramFiles}\hats"
+	}
+
     refreshenv
     echo $env:path
     
