80d3008d298efa7d1782ff04fa7e29616a7364f4
Zui Young <young@youngenius.com>
2017-06-22 16:44:40 +0800

    Add firefox and remove jdk8 python2

--- installer/00-install-prerequisites-chocolatey.ps1
+++ installer/00-install-prerequisites-chocolatey.ps1
@@ -1,7 +1,9 @@
  	Set-ExecutionPolicy Bypass
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    chocolatey install jdk8 nodejs-lts nvda --no-progress -y
-    chocolatey install firefox python2 -x86 --no-progress -y
+    chocolatey install firefox -x86 --no-progress -y
+    chocolatey install googlechrome nodejs-lts --no-progress -y
+    chocolatey upgrade googlechrome nodejs-lts firefox --no-progress -y
+    chocolatey uninstall jdk8 python2 --no-progress -y > $null
     refreshenv
     echo $env:path
     
