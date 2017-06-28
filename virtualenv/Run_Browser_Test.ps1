77206ad043ec77b5df782a47f9f942192d5d0f32
Zui Young <young@youngenius.com>
2017-06-28 15:14:19 +0800

    Fix relative path problem activating robot virtualenv

--- virtualenv/Run_Browser_Test.ps1
+++ virtualenv/Run_Browser_Test.ps1
@@ -1,5 +1,10 @@
 # Set path to hats
 $path_to_hats = "$env:APPDATA\hats"
+echo "Path to hats at $path_to_hats"
+
+# Set current path
+$current_path = (Get-Item -Path ".\" -Verbose).FullName
+echo "Stored current working directory at $current_path"
 
 echo "Set path to JDK for this session"
 $env:Path = "$env:Path;$path_to_hats\jre\bin";
@@ -8,8 +13,11 @@ echo "Set path to browser drivers for this session"
 $env:Path = "$env:Path;$path_to_hats\drivers";
 
 echo "Activate robot virtual environment"
-& "$path_to_hats\robot\Scripts\activate"
+cd "$path_to_hats"
+robot\Scripts\activate
 
+echo "Running Robot on testpage"
+cd "$current_path"
 cd testpage
 .\test.cmd
 
