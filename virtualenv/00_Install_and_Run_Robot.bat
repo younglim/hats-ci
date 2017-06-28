f4130b6ffa7ae4f7a9d7311b996c8af32d6dd178
Zui Young <young@youngenius.com>
2017-06-28 15:26:58 +0800

    Install all dependencies and run robot test

---
+++ virtualenv/00_Install_and_Run_Robot.bat
@@ -0,0 +1,14 @@
+@echo OFF
+pushd %~dp0
+
+echo "Created By younglim - NO WARRANTY PROVIDED"
+echo "=========================================="
+echo.
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\01_Install_Python27_Virtualenv_Robot.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\02_Install_Java.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\03_Install_Browser_Drivers.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\04_Run_Browser_Test.ps1"
+
+pause
+
