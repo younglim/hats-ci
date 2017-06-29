3d97bd31cc53513b6b98d207d6b7f4cd57e20e27
Zui Young <young@youngenius.com>
2017-06-29 17:15:27 +0800

    Delete get-pip.py and pip-install-list.txt

--- virtualenv/05_Cleanup_Create_Shell.ps1
+++ virtualenv/05_Cleanup_Create_Shell.ps1
@@ -17,6 +17,8 @@ $fso.DeleteFile("$path_to_hats\VCForPython27.msi")
 $fso.DeleteFile("$path_to_hats\python27.msi")
 $fso.DeleteFile("$path_to_hats\7z.msi")
 $fso.DeleteFile("$path_to_hats\jre.exe")
+$fso.DeleteFile("$path_to_hats\get-pip.py")
+$fso.DeleteFile("$path_to_hats\pip-install-list.txt")
 
 echo "Copy shell scripts to $path_to_hats"
 Copy-Item "shell\*" "$path_to_hats"
