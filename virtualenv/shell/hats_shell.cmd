6e396256357c6cc57b2d4f73ba008bd621d5360c
Zui Young <young@youngenius.com>
2017-07-05 11:52:39 +0800

    Run hats_shell with parameters

--- virtualenv/shell/hats_shell.cmd
+++ virtualenv/shell/hats_shell.cmd
@@ -1,8 +1,14 @@
 @echo OFF
 pushd %~dp0
 
+if [%1]==[] goto usage
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\hats_shell.ps1" %*
+exit /B 1
+
+:usage
 echo "hats Shell [64-bit] - Created By younglim - NO WARRANTY PROVIDED"
 echo "================================================================"
 echo.
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoExit -ExecutionPolicy Bypass -Command ".\hats_shell.ps1" %*
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoExit -ExecutionPolicy Bypass -Command ".\hats_shell.ps1"
