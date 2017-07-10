387cf7681a1578914cbf197e9cb76ec5066f6b47
Zui Young <young@youngenius.com>
2017-07-10 17:20:22 +0800

    Remove pause from interactive shell

--- virtualenv/shell/hats_shell.cmd
+++ virtualenv/shell/hats_shell.cmd
@@ -11,5 +11,3 @@ echo "================================================================"
 echo.
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NoExit -ExecutionPolicy Bypass -File "%~dp0hats_shell.ps1"
-
-pause
