ce6fcb68058b656c46ee319f1b4a55d01bfe72a1
Zui Young <young@youngenius.com>
2017-07-12 14:45:59 +0800

    Target 64-bit Python for z64 machine

--- virtualenv/01_Install_Python27_Virtualenv_Robot.ps1
+++ virtualenv/01_Install_Python27_Virtualenv_Robot.ps1
@@ -18,7 +18,19 @@ echo "Installing Microsoft Visual C++ Compiler for Python 2.7"
 Start-Process msiexec.exe -ArgumentList "/i `"$path_to_hats\VCForPython27.msi`" /qn" -NoNewWindow -Wait;
 
 echo "Downloading Python 2.7"
-$client.DownloadFile($iniContent["Python"]["Python27-32"],"$path_to_hats\python27.msi");
+
+if ([System.IntPtr]::Size -eq 4) 
+{
+	echo "Your system is 32-bit - Downloading..." 
+	$client.DownloadFile($iniContent["Python"]["Python27-32"],"$path_to_hats\python27.msi");
+
+}	
+else 
+{
+	echo "Your system is 64-bit - Downloading..."
+	$client.DownloadFile($iniContent["Python"]["Python27-64"],"$path_to_hats\python27.msi");
+
+}
 
 echo "Installing Python 2.7"
 Start-Process msiexec.exe -ArgumentList "/a `"$path_to_hats\python27.msi`" /qn TargetDir=`"$path_to_hats\Python27`" PrependPath=0 Include_test=0 DefaultFeature=1" -NoNewWindow -Wait;
@@ -26,7 +38,8 @@ Start-Process msiexec.exe -ArgumentList "/a `"$path_to_hats\python27.msi`" /qn T
 echo "Completed installing Python 2.7"
 
 echo "Set path to Python for this session"
-$env:Path = "$env:Path:$path_to_hats\Python27;$path_to_hats\Python27\Scripts";
+$env:Path = "$env:windir;$env:windir\system32;"
+$env:Path = "$path_to_hats\Python27;$path_to_hats\Python27\Scripts";
 
 echo "Installing pip"
 $client.DownloadFile("https://bootstrap.pypa.io/get-pip.py","$path_to_hats\get-pip.py");
