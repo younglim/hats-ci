935396f87044b3f865a115e19e1f329b196f9af0
Neurrone <dickson@u.nus.edu>
2017-07-13 15:38:27 +0800

    Automatically apply registry fixes required for IE11 web driver

--- virtualenv/00_Install_and_Run_Robot.bat
+++ virtualenv/00_Install_and_Run_Robot.bat
@@ -23,13 +23,15 @@ echo ""
 echo "Created By younglim - NO WARRANTY PROVIDED"
 echo "=========================================="
 echo.
-
+echo Please close all instances of Internet Explorer till the installation completes.
+echo
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\00_Create_Folder_Permissions.ps1"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\01_Install_Python27_Virtualenv_Robot.ps1"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\02_Install_Java.ps1"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\03_Install_Browser_Drivers.ps1"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\04_Cleanup_Create_Shell.ps1"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\05_Run_Browser_Test.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\05_ie_webdriver_config.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\06_Run_Browser_Test.ps1"
 
 pause
 
