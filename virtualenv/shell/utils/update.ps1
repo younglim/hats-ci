c7f31675c4abaa0f559e92a892c31b494ad93c46
Ning Yu <ningyu321@gmail.com>
2017-07-18 17:02:23 +0800

    Forgot to push some changes to compare text files of pip-install-list and pip-install-list-new (just downloaded version) in update.ps1

--- virtualenv/shell/utils/update.ps1
+++ virtualenv/shell/utils/update.ps1
@@ -6,10 +6,10 @@ $url = Get-Content "$path_to_utils\pip-install-list.url"
 echo "URL: $url"
 (New-Object System.Net.WebClient).DownloadFile($url,"$path_to_utils\pip-install-list-new.txt");
 
-$old_list=[System.IO.File]::ReadAllText("utils\pip-install-list.txt")
-$new_list=[System.IO.File]::ReadAllText("utils\pip-install-list-new.txt")
+$old_list="$path_to_utils\pip-install-list.txt"
+$new_list="$path_to_utils\pip-install-list-new.txt"
 
-if (-Not $old_list -ceq $new_list) {
+if ((Get-FileHash $old_list).hash  -ne (Get-FileHash $new_list).hash) {
   echo "New updates found, updating pip packages"
   Remove-Item "$path_to_utils\pip-install-list.txt"
   Rename-Item "$path_to_utils\pip-install-list-new.txt" "pip-install-list.txt"
