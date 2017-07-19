807d812557410112b96b7e6a0693b5fbe891e9c6
jiaminong <jiaminong@outlook.sg>
2017-07-19 17:35:12 +0800

    Download and install appium and windows-build-tools from hats-installer GitHub package.json; segregate Android dependencies installation from Java into another file

--- virtualenv/00_Install_and_Run_Robot.bat
+++ virtualenv/00_Install_and_Run_Robot.bat
@@ -9,17 +9,17 @@ goto check_Permissions
     net session >nul 2>&1
     if %errorLevel% == 0 (
         echo Success: Administrative permissions confirmed.
-		goto run_Application	
+		goto run_Application
     ) else (
         echo Failure: Attempting to run as administrator.
 		powershell "Start-Process cmd -verb runAs '/C cd %cd% && %~nx0'" & exit
-		
+
     )
 
     pause >nul
 
 :run_Application
-echo ""	
+echo ""
 echo "Created By younglim - NO WARRANTY PROVIDED"
 echo "=========================================="
 echo.
@@ -27,9 +27,9 @@ echo.
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\00_Create_Folder_Permissions.ps1"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\01_Install_Python27_Virtualenv_Robot.ps1"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\02_Install_Java.ps1"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\03_Install_Browser_Drivers.ps1"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\04_Cleanup_Create_Shell.ps1"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\05_Run_Browser_Test.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\03_Install_Android_Requirements.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\04_Install_Browser_Drivers.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\05_Cleanup_Create_Shell.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\06_Run_Browser_Test.ps1"
 
 pause
-
