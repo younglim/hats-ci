ad814d0c112ed358e4e383e04de4c1e71b422916
Zui Young <young@youngenius.com>
2017-07-20 15:25:28 +0800

    Reliable file and folder uninstall

--- virtualenv/shell/utils/uninstall ps.ps1
+++ virtualenv/shell/utils/uninstall ps.ps1
@@ -9,3 +9,10 @@ echo "Removing system path entry"
 $path = [System.Environment]::GetEnvironmentVariable('PATH', 'Machine');
 $path = ($path.Split(';') | Where-Object { $_ -ne "$path_to_hats" }) -join ';'
 [System.Environment]::SetEnvironmentVariable('PATH', $path, 'Machine');
+
+echo "Removing all hats files"
+
+cd "$path_to_hats"
+Get-ChildItem *.* -recurse | Where { ! $_.PSIsContainer } | remove-item
+
+pause
--- virtualenv/shell/utils/uninstall.bat
+++ virtualenv/shell/utils/uninstall.bat
@@ -1,27 +1,4 @@
 @echo OFF
 pushd %~dp0
 
-goto check_Permissions
-
-:check_Permissions
-    echo Administrative permissions required. Detecting permissions...
-
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        echo Success: Administrative permissions confirmed.
-		goto run_Application	
-    ) else (
-        echo Failure: Attempting to run as administrator.
-		powershell "Start-Process cmd -verb runAs '/C cd %cd% && %~nx0'" & exit
-		
-    )
-
-    pause >nul
-
-:run_Application
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\uninstall ps.ps1"
-echo "Deleting installed files..."
-cd c:\
-(goto) 2>nul & rmdir "c:\program files\hats" /q /s 
-
-pause
+start powershell -Command "Start-Process powershell \"-ExecutionPolicy Bypass -NoProfile -Command `\"cd \`\"%cd%\`\"; & \`\".\uninstall ps.ps1\`\" `\" \" -Verb RunAs"
