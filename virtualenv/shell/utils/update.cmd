98ff9c8ba55d02b4e76100214bd918a6b9d498fe
Zui Young <young@youngenius.com>
2017-07-21 17:58:23 +0800

    Update run in admin mode

--- virtualenv/shell/utils/update.cmd
+++ virtualenv/shell/utils/update.cmd
@@ -9,9 +9,10 @@ goto check_Permissions
     net session >nul 2>&1
     if %errorLevel% == 0 (
         echo Success: Administrative permissions confirmed.
-        @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\update_ps.ps1"
+        powershell -NoProfile -ExecutionPolicy Bypass -Command ".\update_ps.ps1"
+		pause
     ) else (
         echo Failure: Attempting to run as administrator.
-        echo %CD%
-		    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" –NoProfile -ExecutionPolicy Bypass -Command "Start-Process '%CD%\update.cmd' -Verb runas"
+		powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process '%CD%\update.cmd' -Verb runas"
+		echo Update completed.
     )
