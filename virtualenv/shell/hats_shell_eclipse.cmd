0b54d42e51430daabc27914125bfabff08f9e19c
Zui Young Lim <lim_zui_young@tech.gov.sg>
2017-08-16 14:32:22 +0800

    Update cmdlet for eclipse

--- virtualenv/shell/hats_shell_eclipse.cmd
+++ virtualenv/shell/hats_shell_eclipse.cmd
@@ -2,7 +2,7 @@
 
 if [%1]==[] goto empty_param
 
-set PATH=C:\Program Files\hats\robot/Scripts;C:\Windows;C:\Windows\system32;C:\Windows\system32\WindowsPowerShell\v1.0;C:\Program Files\hats\Python27;C:\Program Files\hats\Python27\Scripts;C:\Program Files\hats\jdk\bin;C:\Program Files\hats\drivers;C:\Program Files\hats\utils;C:\Program Files\hats\nodejs;C:\Program Files\hats\node_modules\.bin;C:\Program Files\hats\npm-global;C:\Program Files\hats\npm-global\bin;C:\Program Files\hats\androidSDK;C:\Program Files\hats\androidSDK\bin;C:\Program Files\hats\androidSDK\platform-tools
+set PATH=C:\Program Files\hats\hats/Scripts;C:\Program Files\hats\androidSDK\emulator;C:\Windows;C:\Windows\system32;C:\Windows\system32\WindowsPowerShell\v1.0;C:\Program Files\hats\Python27;C:\Program Files\hats\Python27\Scripts;C:\Program Files\hats\jdk\bin;C:\Program Files\hats\drivers;C:\Program Files\hats\utils;C:\Program Files\hats\nodejs;C:\Program Files\hats\node_modules\.bin;C:\Program Files\hats\npm-global;C:\Program Files\hats\npm-global\bin;C:\Program Files\hats\androidSDK\tools;C:\Program Files\hats\androidSDK\tools\bin\;C:\Program Files\hats\androidSDK\platform-tools
 
 call %*
 exit /B 1
