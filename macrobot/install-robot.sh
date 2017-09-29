6919da5a9bd684a63b1637ede30b2dbe88131b8d
younglim <younglim>
2017-09-29 16:12:42 +0800

    Fix dest path writing to firefox prefs

--- macrobot/install-robot.sh
+++ macrobot/install-robot.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-# REACT_DIR=$1
-# if [ -z "$1" ]; then
-# 	REACT_DIR="~/bgp-react"
-# fi
+echo -e "\n============================"
+echo -e "hats_shell for Mac installer"
+echo -e "============================\n"
 
-echo "Install brew if currently not installed"
+echo -e "Install brew if currently not installed"
 if [ ! -f /usr/local/bin/brew ]; then
-	echo "	Installing Brew..."
+	echo -e "	Installing Brew..."
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	brew update
 fi
 
-echo "Install Ansible if currenty not installed"
+echo -e "Install Ansible if currenty not installed"
 if [ ! -f /usr/local/bin/ansible-playbook ]; then
-	echo "	Installing Ansible..."
+	echo -e "	Installing Ansible..."
 	brew install ansible
 fi
 
-echo "Running Playbooks"
+echo -e "Running Playbooks"
+
+echo -e "\n===================================="
+echo -e "SUDO password is your login password"
+echo -e "====================================\n"
+
 ansible-playbook -i "localhost," -c local "ansible-playbook-install-robot-framework.yml" --ask-become-pass
+
+echo -e "Source bash_profile"
+source ~/.bash_profile
+
+echo -e "\n============================================================================================================"
+echo -e "Install complete. Please re-open your terminal. In future, run 'hats_shell' to start the testing environment."
+echo -e "============================================================================================================\n"
