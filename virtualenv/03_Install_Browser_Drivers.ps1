c0e13eb0010b17d2a915c5cc867cc8bf08b8d786
younglim <younglim>
2017-10-11 11:25:20 +0800

    Refactor running browser drivers

--- virtualenv/03_Install_Browser_Drivers.ps1
+++ virtualenv/03_Install_Browser_Drivers.ps1
@@ -23,6 +23,7 @@ $client.DownloadFile($iniContent["BrowserDrivers"]["Chrome-32-chrome58"],"$path_
 
 echo "Download IE driver"
 $client.DownloadFile($iniContent["BrowserDrivers"]["IE-32"],"$path_to_hats\IEDriverServer-32.zip");
+$client.DownloadFile($iniContent["BrowserDrivers"]["IE-64"],"$path_to_hats\IEDriverServer-64.zip");
 
 echo "Download Firefox driver"
 $client.DownloadFile($iniContent["BrowserDrivers"]["Firefox-32"],"$path_to_hats\geckodriver-32.zip");
@@ -35,7 +36,8 @@ Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList '
 Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList 'e', '"chromedriver-32-chrome58.zip"', '-o"drivers\chrome-58"', '-aoa' -NoNewWindow -Wait -WorkingDirectory "$path_to_hats"
 
 echo "Unzipping IE driver"
-Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList 'e', '"IEDriverServer-32.zip"', '-o"drivers\ie"', '-aoa' -NoNewWindow -Wait -WorkingDirectory "$path_to_hats"
+Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList 'e', '"IEDriverServer-32.zip"', '-o"drivers\ie32"', '-aoa' -NoNewWindow -Wait -WorkingDirectory "$path_to_hats"
+Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList 'e', '"IEDriverServer-64.zip"', '-o"drivers\ie64"', '-aoa' -NoNewWindow -Wait -WorkingDirectory "$path_to_hats"
 
 echo "Unzipping Firefox driver"
 Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList 'e', '"geckodriver-32.zip"', '-o"drivers\firefox32"', '-aoa' -NoNewWindow -Wait -WorkingDirectory "$path_to_hats"
--- virtualenv/shell/hats_shell_ps.ps1
+++ virtualenv/shell/hats_shell_ps.ps1
@@ -1,9 +1,9 @@
 # Set current path
 $current_path = (Get-Item -Path ".\" -Verbose).FullName
 
-echo "Stored current working directory at $current_path"
+echo "INFO: Stored current working directory at $current_path"
 
-# Set path to hats
+# INFO: Set path to hats
 $path_to_hats  = split-path -parent $MyInvocation.MyCommand.Definition
 
 $path_to_origPrefix = "$path_to_hats\hats\Lib\orig-prefix.txt";
@@ -18,23 +18,51 @@ If (!$pythonDir.equals($origPrefixContent)) {
 
 $env:Path = "$env:windir;$env:windir\system32;$env:windir\system32\WindowsPowerShell\v1.0"
 
-echo "Set path to Python27"
+echo "INFO: Set path to Python27"
 $env:Path = "$env:Path;$path_to_hats\Python27;$path_to_hats\Python27\Scripts";
 
 if ([System.IntPtr]::Size -eq 4)
 {
-	echo "Set path to JDK32 for this session"
+	echo "INFO: Set path to JDK32 for this session"
 	$env:JAVA_HOME = "$path_to_hats\jdk32"
 	$env:Path = "$env:Path;$env:JAVA_HOME\bin";
 }
 else
 {
-	echo "Set path to JDK for this session"
+	echo "INFO: Set path to JDK for this session"
 	$env:JAVA_HOME = "$path_to_hats\jdk"
 	$env:Path = "$env:Path;$env:JAVA_HOME\bin";
 }
 
-echo "Set path to browser drivers for this session"
+echo "INFO: Set path to browser drivers for this session"
+
+. $path_to_hats\utils\Get-ExeTargetMachine.ps1
+
+$ie_path = "C:\Program Files\Internet Explorer\iexplore.exe"
+
+if (Test-Path $ie_path) 
+{
+	$ie_version = (Get-Item $ie_path).VersionInfo.FileVersion
+	
+	if ((Get-ExeTargetMachine $ie_path).TargetMachine -eq "x64")
+	{
+		echo "INFO: Found 64-bit Microsoft Internet Explorer Version $ie_version"
+		# Fallback to 32-bit IE driver for now to workaround slow page typing
+		# $env:Path = "$env:Path;$path_to_hats\drivers\ie64";
+		$env:Path = "$env:Path;$path_to_hats\drivers\ie32";
+	}
+	else
+	{
+		echo "INFO: Found 32-bit Microsoft Internet Explorer Version $ie_version"
+		$env:Path = "$env:Path;$path_to_hats\drivers\ie32";
+	}
+	
+}
+else 
+{
+	echo "WARN: Could not Microsoft internet Explorer"
+	$env:Path = "$env:Path;$path_to_hats\drivers\ie64";
+}
 
 $env:Path = "$env:Path;$path_to_hats\drivers\ie";
 
@@ -50,26 +78,48 @@ if (Test-Path $chrome_path)
 {
 	$chrome_version = (Get-Item $chrome_path).VersionInfo.FileVersion
 
+	echo "INFO: Found Google Chrome Version $chrome_version"
 	if ($chrome_version -match "5[6-8].*") 
 	{
 		echo "Support for Chrome v58 enabled"
 		$env:Path = "$env:Path;$path_to_hats\drivers\chrome-58";
 	}
+	else {
+		$env:Path = "$env:Path;$path_to_hats\drivers\chrome";
+	}
+}
+else
+{
+	echo "WARN: Could not detect Google Chrome"
+	$env:Path = "$env:Path;$path_to_hats\drivers\chrome";
 }
 
+
 $firefox_path = "$path_to_programfiles_x86\Mozilla Firefox\firefox.exe";
-if ((Test-Path $firefox_path) -or ([System.IntPtr]::Size -eq 4))
+if (-Not (Test-Path $firefox_path))
 {
+	$firefox_path = "C:\Program Files\Mozilla Firefox\firefox.exe"
+}
 
-	if (-Not (Test-Path $firefox_path)) 
+if (Test-Path $firefox_path)
+{	
+	$firefox_version = (Get-Item $firefox_path).VersionInfo.FileVersion
+	if ((Get-ExeTargetMachine $firefox_path).TargetMachine -eq "x64")
 	{
-		$firefox_path = "C:\Program Files\Mozilla Firefox\firefox.exe"
-	}
-	
-	if (Test-Path $firefox_path)
+		echo "INFO: Found 64-bit Mozilla Firefox Version $firefox_version"
+		
+		if ($firefox_version -match "[0-5][0-4].*") 
+		{
+			$env:Path = "$env:Path;$path_to_hats\drivers\firefox64-firefox-54";
+		}
+		else
+		{
+			$env:Path = "$env:Path;$path_to_hats\drivers\firefox64";
+		}
+		
+	} 
+	else 
 	{
-		$firefox_version = (Get-Item $firefox_path).VersionInfo.FileVersion
-
 		if ($firefox_version -match "[0-5][0-4].*") 
 		{
 			$env:Path = "$env:Path;$path_to_hats\drivers\firefox32-firefox-54";
@@ -79,59 +129,47 @@ if ((Test-Path $firefox_path) -or ([System.IntPtr]::Size -eq 4))
 			$env:Path = "$env:Path;$path_to_hats\drivers\firefox32";
 		}
 	}
+
 }
 else
 {
-	$firefox_path = "C:\Program Files\Mozilla Firefox\firefox.exe"
-	if (Test-Path $firefox_path)
-	{
-		$firefox_version = (Get-Item $firefox_path).VersionInfo.FileVersion
-
-		if ($firefox_version -match "[0-5][0-4].*") 
-		{
-			$env:Path = "$env:Path;$path_to_hats\drivers\firefox64-firefox-54";
-		}
-		else
-		{
-			$env:Path = "$env:Path;$path_to_hats\drivers\firefox64";
-		}
-	}
-	
+	echo "WARN: Could not detect Mozilla Firefox"
+	$env:Path = "$env:Path;$path_to_hats\drivers\firefox64";
 }
 
-echo "Set path to utils for this session"
+echo "INFO: Set path to utils for this session"
 $env:Path = "$env:Path;$path_to_hats\utils";
 
-echo "Set path to node for this session"
+echo "INFO: Set path to node for this session"
 $env:Path = "$env:Path;$path_to_hats\nodejs";
 
-echo "Set path to node_modules for this session"
+echo "INFO: Set path to node_modules for this session"
 $env:Path = "$env:Path;$path_to_hats\node_modules\.bin;./node_modules/.bin";
 
-echo "Set path to npm-global for this session"
+echo "INFO: Set path to npm-global for this session"
 $env:Path = "$env:Path;$path_to_hats\npm-global;$path_to_hats\npm-global\bin";
 
-echo "Set path to androidSDK tools for this session"
+echo "INFO: Set path to androidSDK tools for this session"
 $env:Path = "$env:Path;$path_to_hats\androidSDK\emulator;$path_to_hats\androidSDK\tools;$path_to_hats\androidSDK\tools\bin\;$path_to_hats\androidSDK\platform-tools";
 
 $env:ANDROID_HOME = "$path_to_hats\androidSDK"
 $env:ANDROID_SDK_HOME = "$path_to_hats\androidSDK"
 $env:ANDROID_SDK_ROOT = "$path_to_hats\androidSDK"
 
-echo "Set path to Git for this session"
+echo "INFO: Set path to Git for this session"
 $env:Path = "$env:Path;$path_to_hats\Git\cmd;$path_to_hats\Git\mingw32\bin;$path_to_hats\Git\usr\bin"
 
-echo "Set path to Gatling for this session"
+echo "INFO: Set path to Gatling for this session"
 $env:Path = "$env:Path;$path_to_hats\Gatling\bin"
 $env:GATLING_HOME = "$path_to_hats\Gatling"
 
-echo "Set path to JMeter for this session"
+echo "INFO: Set path to JMeter for this session"
 $env:Path = "$env:Path;$path_to_hats\JMeter\bin"
 
-echo "Set path to RED for this session"
+echo "INFO: Set path to RED for this session"
 $env:Path = "$env:Path;$path_to_hats\RED"
 
-echo "Activate hats virtual environment"
+echo "INFO: Activate hats virtual environment"
 cd "$path_to_hats"
 hats\Scripts\activate
 
