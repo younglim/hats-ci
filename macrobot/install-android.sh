6ae9036e2f35eded63bcc1eb53d86765778d06e9
younglim <younglim>
2017-11-03 15:54:28 +0800

    Install Android SDK

---
+++ macrobot/install-android.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo -e "\n============================"
+echo -e "Android SDK for Mac installer"
+echo -e "============================\n"
+
+echo -e "Install brew if currently not installed"
+if [ ! -f /usr/local/bin/brew ]; then
+	echo -e "	Installing Brew..."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew update
+fi
+
+echo -e "Install Ansible if currenty not installed"
+if [ ! -f /usr/local/bin/ansible-playbook ]; then
+	echo -e "	Installing Ansible..."
+	brew install ansible
+fi
+
+echo -e "Running Playbooks"
+
+echo -e "\n===================================="
+echo -e "SUDO password is your login password"
+echo -e "====================================\n"
+
+ansible-playbook -i "localhost," -c local "ansible-playbook-install-android.yml" --ask-become-pass
+
+echo -e "Source bash_profile"
+source ~/.bash_profile
+
+echo -e "\n============================================================================================================"
+echo -e "Install complete. Please re-open your terminal. In future, run 'hats_shell' to start the testing environment."
+echo -e "============================================================================================================\n"
