f4de476f53e75e47b3f53a7de2647451a425e72e
younglim <younglim>
2017-11-08 17:59:18 +0800

    Automatically Install Safari WebDriver

---
+++ macrobot/install-safari-webdriver.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo -e "\n=========================="
+echo -e "Install Safari WebDriver"
+echo -e "==========================\n"
+
+echo -e "\n===================================="
+echo -e "Password is your login password"
+echo -e "====================================\n"
+
+sudo echo ""
+
+echo -e "Install brew if currently not installed"
+if [ ! -f /usr/local/bin/brew ]; then
+	echo -e "	Installing Brew..."
+	
+	ruby \
+  	-e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+  	</dev/null
+  	
+  	brew update
+fi
+
+echo -e "Install Ansible if currenty not installed"
+if [ ! -f /usr/local/bin/ansible-playbook ]; then
+	echo -e "	Installing Ansible..."
+	brew install ansible
+fi
+
+echo -e "Running Playbooks"
+
+ansible-playbook -i "localhost," -c local "ansible-playbook-install-safari-webdriver.yml"
+
+. ~/.bash_profile
+
+echo -e "\n============================================================================================================"
+echo -e "Install complete. Check Safari > Preferences > Extensions"
+echo -e "============================================================================================================\n"
