70f098ecef2ea407b391ad9c6224365d86fc6fd7
younglim <younglim@users.noreply.github.com>
2017-11-09 22:54:53 +0800

    Update browsers playbook

---
+++ macrobot/update-browser-drivers.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo -e "\n============================"
+echo -e "Update browser drivers"
+echo -e "============================\n"
+
+echo -e "\n===================================="
+echo -e "Password is your login password"
+echo -e "====================================\n"
+
+sudo echo ""
+
+echo -e "Install brew if currently not installed"
+if [ ! -f /usr/local/bin/brew ]; then
+	echo -e "	Installing Brew..."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew update
+fi
+
+echo -e "Install Ansible if currenty not installed"
+if [ ! -f /usr/local/bin/ansible-playbook ]; then
+	echo -e "	Installing Ansible..."
+	brew install ansible
+fi
+
+echo -e "Running Playbooks"
+
+ansible-playbook -i "localhost," -c local "ansible-playbook-update-browser-drivers.yml"
+
+echo -e "Source bash_profile"
+source ~/.bash_profile
+
+echo -e "\n===================================="
+echo -e "Install complete. Please re-open your terminal."
+echo -e "====================================\n"
