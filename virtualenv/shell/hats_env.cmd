c645e08d8641c4d4b53c563c9605ceda9abf4ab8
younglim <younglim>
2017-12-11 16:15:20 +0800

    Remove ;

--- virtualenv/shell/hats_env.cmd
+++ virtualenv/shell/hats_env.cmd
@@ -1,6 +1,6 @@
 @echo OFF
 
-SET "PATH=%WINDIR%\system32\WindowsPowerShell\v1.0;%WINDIR%;%PROGRAMFILES%\hats;"
+SET "PATH=%WINDIR%\system32\WindowsPowerShell\v1.0;%WINDIR%;%PROGRAMFILES%\hats"
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -command "hats_shell echo `$env:path` | select -Last 1"`) DO (
 SET "PATH=%%F;%PATH%"
