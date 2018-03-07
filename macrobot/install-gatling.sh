738eeaa875179497c1bf917fd396c6ef0be340b5
Zui Young Lim <lim_zui_young@tech.gov.sg>
2018-03-07 15:36:33 +0800

    Update gatling to 2.3.1

---
+++ macrobot/install-gatling.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo -e "\n=========================="
+echo -e "Gatling for Mac installer."
+echo -e "==========================\n"
+
+echo -e "\n===================================="
+echo -e "Password is your login password"
+echo -e "====================================\n"
+
+sudo echo ""
+
+echo -e "Install brew if currently not installed"
+if [ ! -f /usr/local/bin/brew ]; then
+	echo -e "	Installing Brew..."
+	
+	ruby \
+  	-e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+  	</dev/null
+  	
+  	brew update
+fi
+
+echo -e "Install Ansible if currenty not installed"
+if [ ! -f /usr/local/bin/ansible-playbook ]; then
+	echo -e "	Installing Ansible..."
+	brew install ansible
+else
+	brew upgrade ansible
+fi
+
+echo -e "Running Playbooks"
+
+ansible-playbook -i "localhost," -c local "ansible-playbook-install-gatling.yml"
+
+. ~/.bash_profile
+
+echo -e "\n===================================="
+echo -e "Install complete. Please re-open your terminal. In future, run 'gatling' to start."
+echo -e "====================================\n"
