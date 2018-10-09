bf4297f6dcbfc9e988979c4ee15eebeae896a144
younglim <younglim@users.noreply.github.com>
2018-10-09 14:27:10 +0800

    Fix download browser driver issues from GitHub

--- virtualenv/03_Install_Browser_Drivers.ps1
+++ virtualenv/03_Install_Browser_Drivers.ps1
@@ -15,6 +15,7 @@ If(!(test-path "$path_to_hats\drivers"))
 . .\Get-IniContent.ps1
 
 $iniContent = Get-IniContent "config.ini"
+[System.Net.ServicePointManager]::SecurityProtocol =  [Enum]::ToObject([Net.SecurityProtocolType], 3072);
 
 echo "Download Chrome driver"
 $client = new-object System.Net.WebClient;
@@ -25,7 +26,6 @@ $client.DownloadFile($iniContent["BrowserDrivers"]["Chrome-32-chrome58"],"$path_
 
 echo "Download IE driver"
 $client.DownloadFile($iniContent["BrowserDrivers"]["IE-32"],"$path_to_hats\IEDriverServer-32.zip");
-$client.DownloadFile($iniContent["BrowserDrivers"]["IE-64"],"$path_to_hats\IEDriverServer-64.zip");
 
 echo "Download Firefox driver"
 $client.DownloadFile($iniContent["BrowserDrivers"]["Firefox-32"],"$path_to_hats\geckodriver-32.zip");
@@ -41,7 +41,6 @@ Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList '
 
 echo "Unzipping IE driver"
 Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList 'e', '"IEDriverServer-32.zip"', '-o"drivers\ie32"', '-aoa' -NoNewWindow -Wait -WorkingDirectory "$path_to_hats"
-Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList 'e', '"IEDriverServer-64.zip"', '-o"drivers\ie64"', '-aoa' -NoNewWindow -Wait -WorkingDirectory "$path_to_hats"
 
 echo "Unzipping Firefox driver"
 Start-Process -FilePath "$path_to_hats\7-Zip\Files\7-Zip\7z.exe" -ArgumentList 'e', '"geckodriver-32.zip"', '-o"drivers\firefox32"', '-aoa' -NoNewWindow -Wait -WorkingDirectory "$path_to_hats"
